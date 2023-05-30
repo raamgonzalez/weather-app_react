9ef71bd29b39d48210c21b8c43b8ccfa82087e02
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-05-30 12:48:50 -0300

    Fix: error api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import { useState } from 'react';
+import Spinner from './Spinner';
+
 
 const API_KEY = '6ce99c2e52be65aa40de618bb02677c8';
 const API_URL = 'https://api.openweathermap.org/data/2.5/';
+
 
 const dateBuilder = () => {
   const d = new Date();
@@ -36,11 +40,13 @@ const dateBuilder = () => {
   return `${day} ${date} ${month} ${year}`;
 };
 
+
 const App = () => {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
 
   const searchingWeather = (query) => {
     setIsLoading(true);
@@ -66,69 +72,69 @@ const App = () => {
   };
 
   return (
-    <div id='app' className='flex flex-col h-screen text-slate-700 font-serif'>
-      <header className='flex justify-center items-center w-full h-20 text-5xl mt-10 italic'>
-        <h1>Weather App</h1>
-      </header>
-      <form
-        onSubmit={onSubmitSearch}
-        className='search-box flex justify-center items-center w-full max-w-[900px] mt-5 mx-auto px-5 drop-shadow-md shadow-gray-800'
-      >
-        <input
-          type='text'
-          className='search-bar rounded-md text-slate-black py-3 px-5 w-full bg-slate-100 focus:bg-white'
-          placeholder='Buenos Aires, London, Tokyo...'
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-      </form>
-      <main className='flex gap-20 justify-center mt-20'>
-        <section className='w-fit weather-wrap p-5 flex flex-col gap-5 place-items-left text-left w-[500px] text-4xl'>
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : error ? (
-            <p>Error: {error.message}</p>
-          ) : (
-            <>
-              <div className='location-box'>
-                <p className='location text-5xl'>
-                  {weather.name}, {weather.sys.country}
-                </p>
-                <p className='date italic'>{dateBuilder()}</p>
-              </div>
-              <div className='weather-box mt-7'>
-                <p className='temp text-8xl drop-shadow-md'>
-                  {Math.round(weather.main.temp)}°C
-                </p>
-                <p className='weather text-5xl mt-5'>{weather.weather[0].main}</p>
-              </div>
-            </>
-          )}
-        </section>
-        <section>
-          <section className='mt-7 flex flex-col gap-3'>
-            <p className='temp text-3xl font-bold'>
-              <span className='text-4xl font-light'>Humidity:</span> {weather.main.humidity}%
-            </p>
-            <p className='temp text-3xl font-bold'>
-              <span className='text-4xl font-light'>Pressure:</span> {weather.main.pressure} Pa
-            </p>
-            <p className='temp text-3xl font-bold'>
-              <span className='text-4xl font-light'>Feels like:</span> {weather.main.feels_like}°C
-            </p>
-            <p className='temp text-3xl font-bold'>
-              <span className='text-4xl font-light'>Wind speed:</span> {weather.wind.speed} m/s
-            </p>
-            <p className='temp text-3xl font-bold'>
-              <span className='text-4xl font-light'>Longitude:</span> {weather.coord.lon}
-            </p>
-            <p className='temp text-3xl font-bold'>
-              <span className='text-4xl font-light'>Latitude:</span> {weather.coord.lat}
-            </p>
-          </section>
-        </section>
-      </main>
-    </div>
+      <div id='app' className='flex flex-col h-screen text-slate-700 font-serif'>
+        <header className='flex justify-center items-center w-full h-20 text-5xl mt-10 italic'>
+            <h1>Weather App</h1>
+        </header>
+        <form
+          onSubmit={onSubmitSearch}
+          className='search-box flex justify-center items-center w-full max-w-[900px] mt-5 mx-auto px-5 drop-shadow-md shadow-gray-800'
+          >
+          <input
+            type='text'
+            className='search-bar rounded-md text-slate-black py-3 px-5 w-full bg-slate-100 focus:bg-white'
+            placeholder='Buenos Aires, London, Tokyo...'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </form>
+        {isLoading ? (
+              <Spinner/>
+            ) : error ? (
+              <p>Error: {error.message}</p>
+            ) : (
+              <main className='flex flex-col gap-5 justify-center mt-20 p-15  sm:flex sm:flex-row sm:gap-20'>
+                <section className='w-fit weather-wrap p-5 flex flex-col gap-5 place-items-left text-left w-[500px] text-4xl'>
+                    <>
+                      <div className='location-box'>
+                        <p className='location text-5xl'>
+                          {weather.name}, {weather.sys.country}
+                        </p>
+                        <p className='date italic'>{dateBuilder()}</p>
+                      </div>
+                      <div className='weather-box mt-7'>
+                        <p className='temp text-8xl drop-shadow-md'>
+                          {Math.round(weather.main.temp)}°C
+                        </p>
+                        <p className='weather text-5xl mt-5'>{weather.weather[0].main}</p>
+                      </div>
+                    </>
+                </section>
+                <section className='w-fit weather-wrap p-5 flex flex-col gap-5 place-items-left text-left w-[500px] text-4xl sm:p-0 sm:px-5'>
+                      <section className='mt-7 flex flex-col gap-3'>
+                        <p className='temp text-3xl font-bold'>
+                          <span className='text-4xl font-light'>Humidity:</span> {weather.main.humidity}%
+                        </p>
+                        <p className='temp text-3xl font-bold'>
+                          <span className='text-4xl font-light'>Pressure:</span> {weather.main.pressure} Pa
+                        </p>
+                        <p className='temp text-3xl font-bold'>
+                          <span className='text-4xl font-light'>Feels like:</span> {weather.main.feels_like}°C
+                        </p>
+                        <p className='temp text-3xl font-bold'>
+                          <span className='text-4xl font-light'>Wind speed:</span> {weather.wind.speed} m/s
+                        </p>
+                        <p className='temp text-3xl font-bold'>
+                          <span className='text-4xl font-light'>Longitude:</span> {weather.coord.lon}
+                        </p>
+                        <p className='temp text-3xl font-bold'>
+                          <span className='text-4xl font-light'>Latitude:</span> {weather.coord.lat}
+                        </p>
+                      </section>
+                </section>
+              </main>
+        )}
+      </div>
   );
 };
 
---
+++ src/Spinner.jsx
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+export default function Spinner() {
+	return (
+		<div className='text-center mt-10'>
+			<span className="loader"></span>
+		</div>
+	)
+}
