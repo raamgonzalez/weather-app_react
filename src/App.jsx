fb60927b8ff08b27b19c3bd6e7df5e93e55d1729
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-05-30 11:19:21 -0300

    Add: new features

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,6 @@ export default function App() {
   const [weather, setWeather] = useState({})
 
 
-
-  // const fetchWeather = (e) => {
-  //   if (e.key === 'Enter') {
-  //     fetch(`${API_URL}weather?q=${query}&units=metric&appid=${API_KEY}`)
-  //     .then (res => res.json())
-  //     .then (data => {
-  //       return setWeather(data)
-  //     })
-  //   }
-  // }
-
-
-
   function dateBuilder () {
     let d = new Date();
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -36,10 +23,6 @@ export default function App() {
     return `${day} ${date} ${month} ${year}`;
   }
 
-  // const handleChange = (event) => {
-  //   const newSearch = event.target.value
-  //   updateSearch(newSearch)
-  // }
   const searchingWeather = (query) => {
     fetch(`${API_URL}weather?q=${query}&units=metric&appid=${API_KEY}`)
     .then (res => res.json())
@@ -59,14 +42,15 @@ export default function App() {
     searchingWeather(query)
   }
 
-
-
-
+console.log(weather)
 
   return (
     <>
       <div id='app' className="flex flex-col h-screen text-slate-700 font-serif">
-        <form onSubmit={onSubmitSearch} className="search-box flex justify-center items-center w-full max-w-[900px] mt-10 mx-auto px-5 drop-shadow-md shadow-gray-800 ">
+        <header className="flex justify-center items-center w-full h-20 text-5xl mt-10 italic"> 
+          <h1>Weather App</h1>
+        </header>
+        <form onSubmit={onSubmitSearch} className="search-box flex justify-center items-center w-full max-w-[900px] mt-5 mx-auto px-5 drop-shadow-md shadow-gray-800 ">
           <input
           type="text" 
           className="search-bar rounded-md text-slate-black py-3 px-5 w-full bg-slate-100 focus:bg-white" 
@@ -77,8 +61,8 @@ export default function App() {
         <main className="flex gap-20 justify-center mt-20">
             <section className="w-fit weather-wrap  p-5 flex flex-col gap-5 place-items-left text-left w-[500px] text-4xl">
               <div className="location-box ">
-                <div className="location text-5xl">{ weather.name }, {weather.sys.country}</div>
-                <div className="date italic">{ dateBuilder() }</div>
+                <p className="location text-5xl">{ weather.name }, {weather.sys.country}</p>
+                <p className="date italic">{ dateBuilder() }</p>
               </div>
               <div className="weather-box mt-7">
                 <p className="temp text-8xl drop-shadow-md">{ Math.round(weather.main.temp)}°C</p>
@@ -86,15 +70,13 @@ export default function App() {
               </div>
             </section>
             <section>
-              <section className="mt-7">
-                <p className="temp text-5xl">{ weather.weather[0].main }</p>
-                <p className="temp text-4xl">{ weather.weather[0].description }</p>
-                <p className="temp text-4xl">Humidity: { weather.main.humidity }%</p>
-                <p className="temp text-4xl">Pressure: { weather.main.pressure }</p>
-                <p className="temp text-4xl">Feels like: { weather.main.feels_like }°C</p>
-                <p className="temp text-4xl">Wind speed: { weather.wind.speed }</p>
-                <p className="temp text-4xl">Longitude: { weather.coord.lon }</p>
-                <p className="temp text-4xl">Latitude: { weather.coord.lat }</p>
+              <section className="mt-7 flex flex-col gap-3">
+                <p className="temp text-3xl font-bold"><span className='text-4xl font-light'>Humidity:</span> { weather.main.humidity }%</p>
+                <p className="temp text-3xl font-bold"><span className='text-4xl font-light'>Pressure:</span> { weather.main.pressure } Pa</p>
+                <p className="temp text-3xl font-bold"><span className='text-4xl font-light'>Feels like:</span> { weather.main.feels_like }°C</p>
+                <p className="temp text-3xl font-bold"><span className='text-4xl font-light'>Wind speed:</span> { weather.wind.speed } m/s</p>
+                <p className="temp text-3xl font-bold"><span className='text-4xl font-light'>Longitude:</span> { weather.coord.lon }</p>
+                <p className="temp text-3xl font-bold"><span className='text-4xl font-light'>Latitude:</span> { weather.coord.lat }</p>
               </section>
           </section>
         </main>
